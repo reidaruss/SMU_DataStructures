8f6b33dec430bf60a32aef5d60eeda6f55cd4655
Reid Russell <reidr@smu.edu>
2018-09-13 16:37:08 -0500

    Filled vector class with prototypes

--- Sprint2/Project2/dsvector.h
+++ Sprint2/Project2/dsvector.h
@@ -1,4 +1,26 @@
 #ifndef DSVECTOR_H
 #define DSVECTOR_H
 
+template <typename T>
+class DSVector
+{
+private:
+    int size;
+    int capacity;
+    T* data;
+public:
+    DSVector();
+    DSVector(const dsvector<T>&);//Copy Constructor
+    DSVector& operator=(const DSVector& rhs);//Overloaded Assignment Operator
+    ~Vector();//destructor
+
+
+
+    void pushBack(T& val);
+    void remove();
+    void resize();
+}
+
+
+
 #endif // DSVECTOR_H
